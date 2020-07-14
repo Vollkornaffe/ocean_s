854f6ea6c0ab03b4c3bcbd8ba0cae81de05463f8
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-07-14 12:25:35 +0200

    connection between passive particles and boids

--- src/gdboids.h
+++ src/gdboids.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <Godot.hpp>
 #include <Node.hpp>
+#include <PassiveParticles2D.hpp>
 
 namespace godot {
 
@@ -13,16 +14,32 @@ class GDBoids : public Node {
 public:
   static void _register_methods() {
     register_method("_physics_process", &godot::GDBoids::_physics_process);
+    register_method("set_passive_particles", &godot::GDBoids::set_passive_particles);
   }
 
   GDBoids() {}
   ~GDBoids() {}
 
-  void _init() {}
+  void _init() {
+    p_passive_particles = NULL;
+  }
 
   void _physics_process(float delta) {
-    std::cout << "qwer" << std::endl;
+    if (p_passive_particles == NULL) {
+      std::cout << "no passive particles set" << std::endl;
+      return;
+    } 
+
+    std::cout << p_passive_particles->get_amount() << std::endl;
+
   }
+
+  void set_passive_particles(PassiveParticles2D* p) {
+    p_passive_particles = p;
+  }
+
+private:
+  PassiveParticles2D * p_passive_particles;
 };
 
 }
