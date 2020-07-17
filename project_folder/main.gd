cac8267efacca1814310b571edd7af7148c49a85
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-07-17 19:51:07 +0200

    Now using the new animations

--- project_folder/main.gd
+++ project_folder/main.gd
@@ -9,20 +9,22 @@ var t = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Boids").set_passive_particles(get_node("PassiveParticles2D"))
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+#func _process(delta): 
 #	pass
 
 func _process(delta):
 	t += delta
+	var pp = get_node("PassiveParticles2D")
 	if t > 2.0:
-		var pp = get_node("PassiveParticles2D")
 		var amount = pp.get_amount() * 2 
 		pp.set_amount(amount)
 		print(amount)
 		print(Engine.get_frames_per_second())
 		t = 0
+	get_node("GDBoids").write_to_particles(pp)
+
+func _physics_process(delta):
+	get_node("GDBoids").physics_process(delta)
