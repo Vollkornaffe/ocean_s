1767940dc38e678cd55364f66b94719b2492d7cc
Vollkorn Affe <vollkornaffe@gmail.com>
2020-07-22 18:53:04 +0200

    Testing out some ideas

--- project_folder/WindowDialog.gd
+++ project_folder/WindowDialog.gd
@@ -5,11 +5,14 @@ extends WindowDialog
 # var b = "text"
 
 var init = false
+const SpeciesDetails = preload("res://SpeciesDetails.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var species_list = get_node("MarginContainer/ScrollContainer/VBoxContainer")
+	species_list.add_child(SpeciesDetails.instance())
+	species_list.add_child(SpeciesDetails.instance())
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
